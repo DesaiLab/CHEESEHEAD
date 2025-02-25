9ebb713c5e94316a5c7c179150a010a02c298bc5
Bethany Blakely <bethany.j.blakely@gmail.com>
2025-02-25 10:30:55 -0600

    Updated to work with sample data

--- scripts/LST_R/Preprocess_goes_nldas_3.R
+++ scripts/LST_R/Preprocess_goes_nldas_3.R
@@ -5,30 +5,32 @@ library(lubridate)
 library(ncdf4)
 library(ncdf4.helpers)
 
-setwd("/Users/bethanyblakely/Desktop/Analysis/LST")
+homedir<-"/Users/bethanyblakely/Desktop/Analysis/CHEESEHEAD_local/CHEESEHEAD/scripts/LST_R" #working directory
+datadir<-"/Users/bethanyblakely/Desktop/Analysis/LST/Data" #directory where the input data live
+setwd(homedir)
 
 #Set extents #####
-#spatial
-und<-ext(-92,-88, 45, 47)
-big.wi<-ext(-93,-87, 43, 47); med.wi<-ext(-90,-89, 45, 46)
-swth<-vect("ancillary/stei_tree.kml"); swth.utm<-project(swth, "epsg:32616")
-swth2<-buffer(swth, 12000); swth2.utm<-buffer(swth.utm, 12000)
+#list of possible spatial extents
+und<-ext(-92,-88, 45, 47) #underc
+big.wi<-ext(-93,-87, 43, 47); med.wi<-ext(-90,-89, 45, 46) #large bounding boxes for initial cropping (to decrease runtime)
+swth<-vect("stei_tree.kml"); swth.utm<-project(swth, "epsg:32616") #steigerwaldt/treehaven neon AOP, projected to UTM16
+swth2<-buffer(swth, 12000); swth2.utm<-buffer(swth.utm, 12000) #steigerwaldt/treehaven AOP with a 12K buffer, projected to UTM16
 
-wi<-swth2.utm #the one you'll use
+wi<-swth2.utm #the area subset you want the ultimate output in. Must be UTM16
 
 #temporal
-timespan<-c(166:274)#304 #245 #DOYs to do all this for
-#timespan<-c(182:212) #July
+timespan<-c(182:201) #DOYs to do all this for
+
 
 #####
 
-#read in NLDAS: mosaic ####
-setwd("/Users/bethanyblakely/Desktop/Analysis/LST")
-setwd("Data/NLDAS/Incoming2/mosaic")
+#read in, subset, and assign time to NLDAS: mosaic ####
+setwd(datadir)
+setwd("nldas/mosaic") #directory where the nldas data are
 
 files.mo.all<-allfiles<-list.files()
 
-#subset files in date range of interest
+#subset files in date range of interest based on file names
 rdate<-as.POSIXlt(substr(files.mo.all, 22,25), format="%m%d", tz="UTC"); rdoy<-as.numeric(format(rdate, "%j"))
 files.sub<-which(rdoy%in%timespan)
 
@@ -37,20 +39,20 @@ allrast.mo<-list()
 
 for (i in 1:length(files.mo)){
   
-  if(i%%24==0){print(paste("DOY", timespan[i/24]))} #day counter
+  if(i%%24==0){print(paste("DOY", timespan[i/24]))} #day counter for monitoring loop. No other function
   
   file<-files.mo[i]
   
   rst<-terra::rast(file)
-  lst<-project(rst,"epsg:32616")
+  lst<-project(rst,"epsg:32616") #reproject to UTM zone 16
   
-  lst.wisc<-crop(lst, wi,snap="out")
+  lst.wisc<-crop(lst, wi,snap="out") #crop to area of intrest
   plot(lst.wisc)
   
-  datestr<-substr(file, 18,25);timestr<-substr(file, 27,30)
-  ts<-as.POSIXlt(paste(datestr, timestr, sep=""), format="%Y%m%d%H%M",tz="UTC")
+  datestr<-substr(file, 18,25);timestr<-substr(file, 27,30) #subset out date and time from file name
+  ts<-as.POSIXlt(paste(datestr, timestr, sep=""), format="%Y%m%d%H%M",tz="UTC") #...convert to posix
   
-  time(lst.wisc)<-ts
+  time(lst.wisc)<-ts #...and assign as the time dimension for that image"
   
   allrast.mo[[i]]<-lst.wisc
   
@@ -58,12 +60,12 @@ for (i in 1:length(files.mo)){
 
 
 
-brick.mosaic<-rast(allrast.mo)
+brick.mosaic<-rast(allrast.mo) #aggregate into multilayer raster
 #####
 
-#read in NLDAS: noah #####
-setwd("/Users/bethanyblakely/Desktop/Analysis/LST")
-setwd("Data/NLDAS/Incoming2/noah")
+#read in, subset, and assign time to NLDAS: noah. Process identical to mosaic except for different file names#####
+setwd(datadir)
+setwd("nldas/noah")
 
 files.no.all<-allfiles<-list.files()
 
@@ -99,9 +101,9 @@ brick.noah<-rast(allrast.no)
 
 #####
 
-#read in NLDAS: vic #####
-setwd("/Users/bethanyblakely/Desktop/Analysis/LST")
-setwd("Data/NLDAS/Incoming2/vic")
+#read in, subset, and assign time to NLDAS: vic. Same thing again #####
+setwd(datadir)
+setwd("nldas/vic")
 
 files.vic.all<-allfiles<-list.files()
 
@@ -138,38 +140,33 @@ brick.vic<-rast(allrast.vic)
 #####
 
 
-
-#merge NLDAS datasets
+#merge NLDAS datasets - mean and sd
 nldas.brick<-mean(brick.vic, brick.mosaic, brick.noah)
-
-#nldas.sum<-(brick.vic+brick.mosaic+brick.noah)
-#nldas.mean<-nldas.sum/3
-
 nldas.sd<-stdev(brick.vic, brick.mosaic, brick.noah)
+
 #####
 
 
 #read in goes####
   
 
-  setwd("/Users/bethanyblakely/Desktop/Analysis/LST")
-  setwd("Data/goesday")
+  setwd(datadir)
+  setwd("goes")
   
-  #missing a couple files; should have either 720 (for 30 days) or 744 (for 31)
+  #Note: missing a couple files; should have either 720 (for 30 days) or 744 (for 31)
   #actually have 718
   
   #function to correct values for unsigned integer problem: removes stretch,
-  #"converts" raw values to what they would be if read correctlty, then reapplies stretch
+  #"converts" raw values to what they would be if read correctly, then reapplies stretch
   cv<-function(x){
     nval<-((((x-190)/0.0025)+2^16)*0.0025)+190
   }
   
-  hdrs<-list.files(pattern = "*.aux.xml")
-  
+  hdrs<-list.files(pattern = "*.aux.xml") #get list of header files so we can exclude them later
   
   allfiles<-list.files()
   
-  files.goes.all<-setdiff(allfiles, hdrs)
+  files.goes.all<-setdiff(allfiles, hdrs) #exclude hdr files
   
   rdate<-as.POSIXlt(substr(files.goes.all, 24,37), format="%Y%j%H%M%S", tz="UTC"); rdoy<-as.numeric(format(rdate, "%j"))
   
@@ -177,11 +174,11 @@ nldas.sd<-stdev(brick.vic, brick.mosaic, brick.noah)
   
   files.goes<-files.goes.all[files.sub]
   
-  #read in sample file to create target projection for initial clip
+  #read in sample file to reproject bounding box to GOES CRS
   pset<-terra::rast(files.goes[1])
-  bbox<-vect(med.wi)
-  crs(bbox)<-"EPSG:4326"
-  bbox.proj<-project(bbox, pset)
+  bbox<-vect(med.wi) #med.wi is a user-designated (in ~line 15) large lat/lon bounding box that acts as an initial clip for (natively CONUS) goes data
+  crs(bbox)<-"EPSG:4326" #assign WGS84 projection to med.wi
+  bbox.proj<-project(bbox, pset) #reproject this box into goes CRS
   rm(pset)
   
   #main loop
@@ -189,35 +186,33 @@ nldas.sd<-stdev(brick.vic, brick.mosaic, brick.noah)
   par(mfrow=c(2,2))
   for (i in 1:length(files.goes)){
     
-  if(i%%24==0){print(paste("DOY", timespan[i/24]))} #day counter
+  if(i%%24==0){print(paste("DOY", timespan[i/24]))} #Day counter. Not necessary
   
   grst<-terra::rast(files.goes[i])
-  
-
+  #plot(grst) #entire CONUS, LST and QC ("DQF"). Slows down loop a lot.
   
   grst.crop<-crop(grst, bbox.proj)
   grst.utm<-project(grst.crop,"epsg:32616", method="near") #project to UTM zone 16
   grst.wi<-crop(grst.utm, swth2.utm)
+  #plot(grst.wi) #cropped to user-selected large bounding box
   
-  
-  
+  #separate out LST and QC
   glst.wi<-grst.wi$LST
   gqc.wi<-grst.wi$DQF
-  
   
   #apply function fixing unsigned integer problem
   glst.wi<-cv(glst.wi)
   
+  #NaN pixels with QC != best quality (leaves only cloud-free pixels) or values below 280K (~7C)
   glst.wi[gqc.wi!=0]<-NA
   glst.wi[glst.wi<280]<-NA
   
+  #extract and assign time dimension
   filetime<-substr(files.goes[i], 24, 37)
-  #filetime<-substr(files.goes[i], 48, 61)
   ts.posix<-as.POSIXct(filetime, format="%Y%j%H%M%S", tz="UTC")
- 
   time(glst.wi)<-ts.posix
   
-  
+  ##little plotting loop if you want to see how it's working
   #if(!all(is.nan(glst.wi))){
   #plot(glst.wi, main=as.character(time(glst.wi)))
   #plot(gqc.wi, main="qc")
@@ -226,20 +221,18 @@ nldas.sd<-stdev(brick.vic, brick.mosaic, brick.noah)
   allrast.goes[[i]]<-glst.wi
   }
   
+  #combine into multilayer raster
   brick.goes<-rast(allrast.goes)
 
 #####
     
 
 #Resample and realign#####
-goesdat<-disagg(brick.goes, fact=2, method="near"); #why does Ankur use NN here?
+goesdat<-disagg(brick.goes, fact=2, method="near")
 nldasdat<-project(nldas.brick, goesdat, method="near")
 
-##This currently breaks R  
-#setwd("/Users/bethanyblakely/Desktop/Analysis/LST")
-#writeRaster(goesdat, "GOES_July_2019.nc", overwrite=TRUE)
-#writeRaster(nldasdat, "NLDAS_July_2019.nc", overwrite=TRUE)
-
+#cleanup
 rm("allrast.mo", "allrast.no", "allrast.goes", "allrast.vic","brick.goes", "brick.mosaic", "brick.noah", "brick.vic")
+
 #####
   
